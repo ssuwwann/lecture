b0f1b9fb38e9464470004b6b0f7ff3207468ac15
whdygks486 <whdygks486@naver.com>
2024-08-23 17:25:21 +0900

    메인페이지 첫번째 업로드

---
+++ src/main/java/org/threefour/homelearn/home/domain/HomeSearchVO.java
@@ -0,0 +1,17 @@
+package org.threefour.homelearn.home.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HomeSearchVO {
+
+  private String keyword;
+  private String search;
+
+}
